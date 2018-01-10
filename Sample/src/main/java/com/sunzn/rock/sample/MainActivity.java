96da07efcab7647e43f6526d399b5bdc517bdd54
sunzn <sunzn.cn@gmail.com>
2018-01-10 14:55:26 +0800

    Update Sample

--- Sample/src/main/java/com/sunzn/rock/sample/MainActivity.java
+++ Sample/src/main/java/com/sunzn/rock/sample/MainActivity.java
@@ -1,13 +1,13 @@
 package com.sunzn.rock.sample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
 import com.sunzn.rock.library.RackVew;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private RackVew mRackVew;
 
